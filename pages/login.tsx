a438310fb5921c3748966cc4041d8f39a68027b7
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-17 13:00:53 -0400

    Custom font

---
+++ pages/login.tsx
@@ -0,0 +1,19 @@
+export default function Example() {
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-50">
+        <body class="h-full">
+        ```
+      */}
+      <div className="grid h-screen place-items-center">
+        <h1 className="font-outfit text-5xl font-[500] text-slate-800">
+          Welcome to the Weather App
+        </h1>
+      </div>
+    </>
+  )
+}
